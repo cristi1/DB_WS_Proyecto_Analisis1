e85e6b92feef199cae81a5113c58a9d96782eebb
Cristian Azurdia <201020331@ingenieria.usac.edu.gt>
2017-04-22 23:50:15 -0600

    Version 0.5
    -Separación en clase la conexión a la base de datos
    -Separación en clase las operaciones realizadas al professor
    -Separación en clase de las pruebas unitarias

--- WBSV_AYD1/BD_Connect.py
+++ WBSV_AYD1/BD_Connect.py
@@ -1,4 +1,20 @@
 __author__ = 'crist'
 
+import WBSV_AYD1
+from flask.ext.mysql import MySQL
 
+class BD():
 
+    WBSV_AYD1.config['MYSQL_DATABASE_USER'] = 'root'
+    WBSV_AYD1.app.config['MYSQL_DATABASE_PASSWORD'] = 'ayd2017'
+    WBSV_AYD1.app.config['MYSQL_DATABASE_DB'] = 'proyectoClase'
+    WBSV_AYD1.app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+
+    __mysql = None
+
+    def __init__(self):
+        self.mysql = MySQL()
+        self.mysql.init_app(WBSV_AYD1.app)
+
+    def get_mysql(self):
+        return self.__mysql
