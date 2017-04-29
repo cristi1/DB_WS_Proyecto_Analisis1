0f246777d975a571064dff464b6cb76cb0bfa9f8
cristi1 <cristi1@users.noreply.github.com>
2017-04-29 11:50:10 -0600

    Add files via upload
    actualizacion de base de datos,
    nuevos campos en tabla curso.

--- bd.sql
+++ bd.sql
@@ -1,5 +1,4 @@
 create database proyectoClase;
-
 use proyectoClase;
 
 create table catedratico(
@@ -9,3 +8,21 @@ create table catedratico(
     contrasenia varchar(50),
     constraint codigo primary key (codigo)
 );
+
+create table curso(
+	codigo int not null auto_increment,
+    nombre varchar(100) not null,
+    -- codigo_asistencia varchar(5) not null, generado en el WS
+    constraint codigo primary key (codigo)
+);
+
+create table asistencia (
+	numero int not null auto_increment,
+	id_alumno varchar(10) not null,
+    fecha date not null,
+    codigo_asistencia varchar(5) not null,
+    constraint numero primary key (numero)
+);
+
+alter table catedratico_curso add fecha date not null;
+alter table curso add codigo_asistencia varchar(5) not null;
